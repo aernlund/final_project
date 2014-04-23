5c77b6e3129dbc0d1c2f6ccb27a8613bb95dbb17
aernlund <awe220@nyumc.org>
2014-04-22 22:18:26 -0400

    2nd attempt bin works one file

--- bin.py
+++ bin.py
@@ -2,35 +2,39 @@
 
 import numpy as np
 
-def processfile(filechromdict, chromlist, start, segments):
+def processbins(filechromdict, chromlist, start, segments):
     """ process chromosome dict and chromlist to produce segments 
         that are binned and averaged over bin
     """
     chrombinaverage = []
-    for key,value in filechromdict.iteritems():
+    for key, value in filechromdict.iteritems():
         startchrom = []
-        endchrom = []
         segmentchrom = []
-        for i,row in enumerate(chromlist):
+        for i, row in enumerate(chromlist):
             if key == row:
                 startchrom.append(start[i])
                 segmentchrom.append(segments[i])
         startchrom = np.array(startchrom).astype(float)
         segmentchrom = np.array(segmentchrom).astype(float)
-        bins = np.linspace(0, value , 5)
+        bins = np.linspace(0, value, 5)
         digitized = np.digitize(startchrom, bins)
         means = []
+        keylist = []
         for i in range(1, len(bins)):
             temp = []
-            for index,value in enumerate(digitized):
+            keylist.append(key)
+            for index, value in enumerate(digitized):
                 if i == value:
                     temp.append(segmentchrom[index])
             if temp:
                 means.append([bins[i]] + [np.array(temp).mean()])
             else:
                 means.append([bins[i]] + [np.nan])
-        chrombinaverage.append([key] + means)        
-    return(chrombinaverage)
+        print keylist
+        print len(means)
+        for i, item in enumerate(keylist):
+            chrombinaverage.append([item] + means[i])        
+    return chrombinaverage
 
 testfile = '/Users/iManda/Desktop/final_project/GAMED_p_TCGA_B_312_313_314_NSP_GenomeWideSNP_6_E10_1361620.hg18.seg.txt'
 officialchrom = '/Users/iManda/Desktop/final_project/chrom_bps.txt'
@@ -50,4 +54,4 @@ chromlist = [row[1] for row in cnafile]
 start = [row[2] for row in cnafile]
 segments = [row[-1] for row in cnafile]
 
-processed = processfile(filechromdict, chromlist, start, segments)
+processed = processbins(filechromdict, chromlist, start, segments)
